9df7a4d6fa528d3154b98fb2fa0b1900ad9ce807
Nityoday Tekchandani <32616539+nityoday@users.noreply.github.com>
2022-09-15 00:32:18 +0530

    Home

--- server.js
+++ server.js
@@ -1,1 +1,20 @@
-const express = require('express');
+const express = require('express'); 
+const app = express()
+const PORT = process.env.PORT || 3000   
+const ejs = require('ejs');
+const expressLayout = require('express-ejs-layouts');
+const path = require('path');
+// if we don't have a port from env file then we use 3000, else env is used. 
+
+app.get('/', (req,res) => {
+    res.render('home')
+})
+
+//set template engine
+app.use(expressLayout)
+app.set('views', path.join(__dirname, '/resources/views'))
+app.set('view engine', 'ejs');
+
+app.listen(PORT, () => {
+    console.log(`Listening on port xyz ${PORT}`); 
+}) 
