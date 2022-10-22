4eb0302e5d16353cb2675abe73d07907344681a7
Nityoday Tekchandani <32616539+nityoday@users.noreply.github.com>
2022-10-22 16:16:37 +0530

    code

--- public/js/app.js
+++ public/js/app.js
@@ -7,11 +7,15 @@
   \*****************************/
 /***/ (() => {
 
-console.log('Hello from App JS');
+console.log('Hello from App JS'); // client side. the console logs here are dispplayed in the browser. 
+
 var addToCart = document.querySelectorAll('.add-to-cart');
 addToCart.forEach(function (btn) {
   btn.addEventListener('click', function (e) {
+    var pizza = JSON.parse(btn.dataset.pizza); // this comes from data-pizza from button in home.ejs 
+
     console.log(e);
+    console.log(pizza);
   });
 }); //  this automatically compiles and goes into the public folder due to laravel mix.
 
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,9 +1,13 @@
 console.log('Hello from App JS');
+// client side. the console logs here are dispplayed in the browser. 
 let addToCart = document.querySelectorAll('.add-to-cart')
 
 addToCart.forEach((btn) => {
     btn.addEventListener('click', (e) => {
+        let pizza = JSON.parse(btn.dataset.pizza)
+        // this comes from data-pizza from button in home.ejs 
         console.log(e)
+        console.log(pizza)
     })
 })
 //  this automatically compiles and goes into the public folder due to laravel mix. 
