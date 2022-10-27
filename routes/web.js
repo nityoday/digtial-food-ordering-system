a080bcb9bc91c2ea6a69dc6c01d6c26ae99366c4
Nityoday Tekchandani <32616539+nityoday@users.noreply.github.com>
2022-10-27 21:31:44 +0530

    Update web.js

--- routes/web.js
+++ routes/web.js
@@ -8,7 +8,7 @@ const cartController = require('../app/http/controllers/customers/cartController
     app.get('/register', authController().register)
     app.get('/login', authController().login)
     //post events should be sent to controller as this can be large 
-    
+    app.post('/register', authController().postRegister)
     
     
     
